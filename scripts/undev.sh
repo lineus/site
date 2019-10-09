a26b291d33ad9c387024eae8a98129a93867747a
Kevin Huff <lineus@gmail.com>
2019-10-09 16:02:36 -0400

    undev - to stop nodemon & server.js

--- scripts/undev.sh
+++ scripts/undev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-n=$(ps auxwww | grep -i nodemon | grep -v grep | awk '{ print $2 }');
-s=$(ps auxwww | grep scripts\/server | grep -v grep | awk '{ print $2 }');
+n=$(ps auxwww | grep -i nodemon |  awk '!/grep/ { print $2 }');
+s=$(ps auxwww | grep scripts\/server |  awk '!/grep/ { print $2 }');
 
 deadN=$(kill ${n} 2>/dev/null; echo $?);
 deadS=$(kill ${s} 2>/dev/null; echo $?);
