19b3185878e17249b3ffed1201d0c36083ac4cf6
Kevin Huff <lineus@gmail.com>
2019-10-24 08:40:05 -0400

    fixed broken awk range for killing browser-sync

--- scripts/undev.sh
+++ scripts/undev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 n=$(ps auxwww | grep -i nodemon |  awk '!/grep/ { print $2 }');
-s=$(ps auxwww | grep -i browser-sync |  awk '!/grep/ { print $2 }');
+s=$(ps auxwww | grep browser-sync |  awk '!/grep brow/ { print $2 }');
 
 deadN=$(kill ${n} 2>/dev/null; echo $?);
 deadS=$(kill ${s} 2>/dev/null; echo $?);
